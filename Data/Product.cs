64058a9f2c6ddb31d788e5c79a1cd12638932f08
HAO LUONG <luongvihao@outlook.com>
2026-02-07 22:02:22 +0700

    13.107_Product Repository & Components

--- Data/Product.cs
+++ Data/Product.cs
@@ -7,6 +7,7 @@ namespace YumBlazor.Data
     {
         public int Id { get; set; }
         [Required]
+        public string Name { get; set; }
         public decimal Price { get; set; }
         [Range(0.01, 1000)]
         public string? Description { get; set; }

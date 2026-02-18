5c0adc89a975e5f58a8c713595e40ac49eff1b6b
HAO LUONG <luongvihao@outlook.com>
2026-02-18 20:38:19 +0700

    16.146_Order Header & Details Model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,6 +9,9 @@ namespace YumBlazor.Data
         public DbSet<Product> Product { get; set; }
         public DbSet<ShoppingCart> ShoppingCart { get; set; }
 
+        public DbSet<OrderHeader> OrderHeader { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
